eaaec608e807dae87a7fd2068d72fb826fb0cd9d
Matthew Kaufmann <matthew.e.kaufmann@gmail.com>
2022-03-08 21:05:09 -0600

    Combined with prior mapping file

--- mapping.py
+++ mapping.py
@@ -37,6 +37,84 @@ census_data = pd.read_csv("data/census_demos.csv")
 census_data["cca_num"] = census_data["cca_num"].astype(str)
 geojson = gpd.read_file("data/community_areas.geojson")
 
+# Create dictionary needed for multi-layer drop level
+all_options = {
+    'Race': {"White": "White",
+             "Black": "Black_or_African_American",
+             "Native American": "American_Indian_or_Alaska_Native",
+             "Asian": "Asian",
+             "Native Hawaiian or Other Pacific Islander": "Native_Hawaiian_or_Other_Pacific_Islander",
+             "Other single race": "some_other_race_alone",
+             "2+ races": "two_or_more_races"},
+    'Min annual income': {"$0": "LTM_income_sub_10k",
+                         "$10k": "LTM_income_10-15k",
+                         "$15k": "LTM_income_15_20k",
+                         "$20k": "LTM_income_20_25k",
+                         "$25k": "LTM_income_25_30k",
+                         "$30k": "LTM_income_30_35k",
+                         "$35k": "LTM_income_35_40k",
+                         "$40k": "LTM_income_40_45k",
+                         "$45k": "LTM_income_45_50k",
+                         "$50k": "LTM_income_50_60k",
+                         "$60k": "LTM_income_60_75k",
+                         "$75k": "LTM_income_75_100k",
+                         "$100k": "LTM_income_100_125k",
+                         "$125k": "LTM_income_125_150k",
+                         "$150k": "LTM_income_150_200k",
+                         "$200k": "LTM_income_200k+"},
+    #'Income': 
+    # 
+             #{'Min': [("$0", "LTM_income_sub_10k"),
+    #                    ("$10k", "LTM_income_10-15k"),
+    #                    ("$15k", "LTM_income_15_20k"),
+    #                    ("$20k", "LTM_income_20_25k"),
+    #                    ("$25k", "LTM_income_25_30k"),
+    #                    ("$30k", "LTM_income_30_35k"),
+    #                    ("$35k", "LTM_income_35_40k"),
+    #                    ("$40k", "LTM_income_40_45k"),
+    #                    ("$45k", "LTM_income_45_50k"),
+    #                    ("$50k", "LTM_income_50_60k"),
+    #                    ("$60k", "LTM_income_60_75k"),
+    #                    ("$75k", "LTM_income_75_100k"),
+    #                    ("$100k", "LTM_income_100_125k"),
+    #                    ("$125k", "LTM_income_125_150k"),
+    #                    ("$150k", "LTM_income_150_200k"),
+    #                    ("$200k", "LTM_income_200k+")],
+    #            'Max': [("$10k", "LTM_income_sub_10k"),
+    #                   ("$15k", "LTM_income_10-15k"),
+    #                   ("$20k", "LTM_income_15_20k"),
+    #                   ("$25k", "LTM_income_20_25k"),
+    #                   ("$30k", "LTM_income_25_30k"),
+    #                   ("$35k", "LTM_income_30_35k"),
+    #                   ("$40k", "LTM_income_35_40k"),
+    #                   ("$45k", "LTM_income_40_45k"),
+    #                   ("$50k", "LTM_income_45_50k"),
+    #                   ("$60k", "LTM_income_50_60k"),
+    #                   ("$75k", "LTM_income_60_75k"),
+    #                   ("$100k", "LTM_income_75_100k"),
+    #                   ("$125k", "LTM_income_100_125k"),
+    #                   ("$150k", "LTM_income_125_150k"),
+    #                   ("$200k", "LTM_income_150_200k"),
+    #                   ("No max", "LTM_income_200k+")]},
+    'Unemployment': {"Percent unemployed": "percent_unemployed"}
+    }
+maximum_income_d = {"$10k": "LTM_income_sub_10k",
+                    "$15k": "LTM_income_10-15k",
+                    "$20k": "LTM_income_15_20k",
+                    "$25k": "LTM_income_20_25k",
+                    "$30k": "LTM_income_25_30k",
+                    "$35k": "LTM_income_30_35k",
+                    "$40k": "LTM_income_35_40k",
+                    "$45k": "LTM_income_40_45k",
+                    "$50k": "LTM_income_45_50k",
+                    "$60k": "LTM_income_50_60k",
+                    "$75k": "LTM_income_60_75k",
+                    "$100k": "LTM_income_75_100k",
+                    "$125k": "LTM_income_100_125k",
+                    "$150k": "LTM_income_125_150k",
+                    "$200k": "LTM_income_150_200k",
+                    "No max": "LTM_income_200k+"}
+
 # -----------------------------------------------------------
 # App layout
 
@@ -47,17 +125,13 @@ app.layout = dbc.Container([
 
     dbc.Row([
         dbc.Col([
-            dcc.Dropdown(id="select_race",
+            dcc.Dropdown(id="select_first_filter",
                         options =[
-                            {"label": "White", "value": "White"},
-                            {"label": "Black", "value": "Black_or_African_American"},
-                            {"label": "Native American", "value": "American_Indian_or_Alaska_Native"},
-                            {"label": "Asian", "value": "Asian"},
-                            {"label": "Native_Hawaiian_or_Other_Pacific_Islander", "value": "Native_Hawaiian_or_Other_Pacific_Islander"},
-                            {"label": "Other single race", "value": "some_other_race_alone"},
-                            {"label": "2+ races", "value": "two_or_more_races"}],
+                            {"label": "Race", "value": "Race"},
+                            {"label": "Minimum annual income", "value": "Min annual income"},
+                            {"label": "Unemployment", "value": "Unemployment"}],
                         multi = False,
-                        value = "White",
+                        value = "Race",
                         style = {'width': '40%', 'display': 'inline-block', 'text-align': 'center', 'font-family': 'Helvetica'}
                         ),
 
@@ -74,7 +148,11 @@ app.layout = dbc.Container([
                         multi = False,
                         value = "White",
                         style = {'width': '40%', 'display': 'inline-block', 'text-align': 'center', 'font-family': 'Helvetica'}
-                        )
+                        ),
+            # Graph 1 second-level dropdown
+            dcc.Dropdown(id="secondary_filter",
+                         style = {'width': '40%', 'display': 'inline-block', 'text-align': 'center'}
+                         ),
 
             ])
 
@@ -95,10 +173,39 @@ app.layout = dbc.Container([
 # -----------------------------------------------------------
 # Connect the Plotly graphs with Dash Componenets
 @app.callback(
+    dash.dependencies.Output('secondary_filter', 'options'),
+    [dash.dependencies.Input('select_first_filter', 'value')]
+    )
+
+# def get_data(option):
+#     '''
+#     Updates data frame for filtering values
+#     '''
+#     if option == 'Race' or option == 'Unemployment':
+#         output = census_data[['cca_name', 'cca_']
+
+def set_second_options(option):
+    '''
+    Updates secondary filter options
+    '''
+    return [{'label': k, 'value': v} for k,v in all_options[option].items()]
+
+@app.callback(
+    dash.dependencies.Output('secondary_filter', 'value'),
+    [dash.dependencies.Input('secondary_filter', 'options')]
+    )
+
+def set_second_value(options):
+    '''
+    Updates secondary default value
+    '''
+    return options[0]['value']
+
+@app.callback(
     [Output(component_id = 'demo_map', component_property='figure'),
     Output(component_id = 'demo_map2', component_property='figure')],
 
-    [Input(component_id = 'select_race', component_property='value'),
+    [Input(component_id = 'secondary_filter', component_property='value'),
     Input(component_id = 'select_race2', component_property='value')]
     )
 
