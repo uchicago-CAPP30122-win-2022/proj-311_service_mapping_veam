3564b466d0116d7a15512aac0b14910223bc4cb5
v4vigtory <41167119+v4vigtory@users.noreply.github.com>
2022-03-12 06:10:41 -0600

    Removed limits from sr_data_collector code

--- servicemapping/data_pull/311_sr/sr_data_collector.py
+++ servicemapping/data_pull/311_sr/sr_data_collector.py
@@ -5,7 +5,6 @@ File to pull 311 Service Request data for 2019-2021 from Chicago Data Portal
 import pandas as pd
 from sodapy import Socrata
 
-
 def retrieve_data():
     
     # Data Portal details
@@ -14,21 +13,52 @@ def retrieve_data():
     app_token = "6tRoBirkYQMdr8MMFR8FzgBXq"
     api_username = "v4vigtory@gmail.com"
     api_password = "311_TeamVeam"
-    client = Socrata(socrata_domain, app_token, username=api_username, password=api_password)
+    client = Socrata(socrata_domain, app_token, 
+                    username=api_username, 
+                    password=api_password)
     
     # breaking up data collection by year to modularize API pull 
     sr_2021 = client.get(socrata_dataset_identifier,
-                     select = "sr_number, sr_type, sr_short_code, owner_department, status, created_date, closed_date, date_extract_y(created_date) as year, street_address, city, state, zip_code, community_area, location",
-                     where = "date_extract_y(created_date) = 2021 AND community_area IS NOT NULL AND sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')", 
-                     limit = 1000000)
+                     select = '''
+                        sr_number, sr_type, sr_short_code, 
+                        owner_department, status, 
+                        created_date, closed_date, 
+                        date_extract_y(created_date) as year, 
+                        street_address, city, state, zip_code, 
+                        community_area, location''',
+                     where = '''
+                        date_extract_y(created_date) = 2021 AND 
+                        community_area IS NOT NULL AND 
+                        sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')'''
+                     )
+    
     sr_2020 = client.get(socrata_dataset_identifier,
-                     select = "sr_number, sr_type, sr_short_code, owner_department, status, created_date, closed_date, date_extract_y(created_date) as year, street_address, city, state, zip_code, community_area, location",
-                     where = "date_extract_y(created_date) = 2020 AND community_area IS NOT NULL AND sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')", 
-                     limit = 1000000)
+                     select = '''
+                        sr_number, sr_type, sr_short_code, 
+                        owner_department, status, 
+                        created_date, closed_date, 
+                        date_extract_y(created_date) as year, 
+                        street_address, city, state, zip_code, 
+                        community_area, location''',
+                     where = '''
+                        date_extract_y(created_date) = 2020 AND 
+                        community_area IS NOT NULL AND 
+                        sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')''' 
+                     )
+    
     sr_2019 = client.get(socrata_dataset_identifier,
-                     select = "sr_number, sr_type, sr_short_code, owner_department, status, created_date, closed_date, date_extract_y(created_date) as year, street_address, city, state, zip_code, community_area, location",
-                     where = "date_extract_y(created_date) = 2019 AND community_area IS NOT NULL AND sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')", 
-                     limit = 1000000)
+                     select = '''
+                        sr_number, sr_type, sr_short_code, 
+                        owner_department, status, 
+                        created_date, closed_date, 
+                        date_extract_y(created_date) as year, 
+                        street_address, city, state, zip_code, 
+                        community_area, location''',
+                     where = '''
+                        date_extract_y(created_date) = 2019 AND 
+                        community_area IS NOT NULL AND 
+                        sr_type NOT IN ('311 INFORMATION ONLY CALL', 'Aircraft Noise Complaint')'''
+                     )
     
     # convert to pandas dataframe
     sr_2021_df = pd.DataFrame(sr_2021)
