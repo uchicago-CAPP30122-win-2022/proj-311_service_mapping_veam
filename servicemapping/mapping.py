dc236b1f5ce28cf6fa7ae0992dd1985b1104c497
Matt Kaufmann <mkaufmann1@uchicago.edu>
2022-03-15 22:30:43 -0500

    Removed wildcard imports on mapping.py

--- servicemapping/mapping.py
+++ servicemapping/mapping.py
@@ -9,10 +9,10 @@ Set overall app layout for 311 Service Mapping Project
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
-from webapp.graphs.graph_map1 import *
-from webapp.graphs.graph_map2 import *
-from webapp.graphs.graph_bars import *
-from webapp.graphs.graph_scatter import *
+from webapp.graphs.graph_map1 import demo_map
+from webapp.graphs.graph_map2 import resolution_times_graph
+from webapp.graphs.graph_bars import middle_row_content
+from webapp.graphs.graph_scatter import bottom_row_content
 from maindash import app # ACTION: this can be deleted when transferred to __main__
 
 
