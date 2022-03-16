db023d97f8da6dbf75aa3ec806bc9d65dc4b2077
Matthew Kaufmann <matthew.e.kaufmann@gmail.com>
2022-03-15 22:29:20 -0500

    Updating mapping, need to plyint

--- servicemapping/mapping.py
+++ servicemapping/mapping.py
@@ -6,25 +6,9 @@ App Layout
 Set overall app layout for 311 Service Mapping Project
 '''
 
-
-# -----------------------------------------------------------
-# Import statements
-# QUESTION/ACTION: CHECK WHAT WE DO OR DO NOT NEED TO KEEP
-
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
-
-# TO DELETE
-# from dash.dependencies import Input, Output
-# from dash import dcc
-# import plotly.express as px
-# import pandas as pd
-# import geopandas as gpd
-# import numpy as np
-# import statsmodels
-# from data_pull.census.create_cca_tract_dict import create_dictionaries
-
 from webapp.graphs.graph_map1 import *
 from webapp.graphs.graph_map2 import *
 from webapp.graphs.graph_bars import *
@@ -34,6 +18,9 @@ from maindash import app # ACTION: this can be deleted when transferred to __mai
 
 # -----------------------------------------------------------
 # App Layout
+
+source_note = "Sources: Chicago 311 Service Request Data (Chicago Data Portal)\
+               & American Community Survey (2019)"
 
 app.layout = dbc.Container([
     html.Br(),
@@ -45,7 +32,7 @@ app.layout = dbc.Container([
     dbc.Row(middle_row_content),
     html.Br(),
     dbc.Row(bottom_row_content),
-    dbc.Row("Sources: Chicago 311 Service Request Data (Chicago Data Portal) & American Community Survey (2019)"),
+    dbc.Row(source_note),
     dbc.Row(html.Br())
     ]
     ,fluid=True, style={'backgroundColor':'#0f2537'}
